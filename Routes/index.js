9f5be3a203957d5e5cd278163bb1f7b089d26b4c
Carlos <perez.carlos_a@yahoo.com>
2021-08-13 22:51:10 -0700

    more changes

--- Routes/index.js
+++ Routes/index.js
@@ -3,8 +3,8 @@ const apiRoutes = require("./API");
 const path = require("path");
 
 router.use("/api", apiRoutes);
-router.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// router.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 module.exports = router;
