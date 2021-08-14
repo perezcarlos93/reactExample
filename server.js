1eb2cd6fe1691f4f26f2d8253c783670fb46e8ef
Carlos <perez.carlos_a@yahoo.com>
2021-08-13 23:24:50 -0700

    changes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
 const mongoose = require("mongoose");
 const routes = require("./Routes");
 
 const PORT = process.env.PORT || 3001;
+require("dotenv").config();
 
 mongoose.connect(
 	process.env.MONGO_DB_URL || "mongodb://localhost:27017/example",
@@ -20,9 +20,9 @@ app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-	});
+	// app.get("*", (req, res) => {
+	// 	res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	// });
 }
 
 app.use(routes);
