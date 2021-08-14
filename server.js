eda77a85ad152a38b000596cd7fee3eb4480817c
Carlos <perez.carlos_a@yahoo.com>
2021-08-13 22:39:49 -0700

    changes

--- server.js
+++ server.js
@@ -19,11 +19,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
-
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
 }
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
+
+app.use(routes);
 
 app.listen(PORT, () =>
 	console.log(`App is listening on http://localhost:${PORT}`)
