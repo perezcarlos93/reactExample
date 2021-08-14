eee0eb37957a2e5d1a55b07649bc8bf084d34b2f
Carlos <perez.carlos_a@yahoo.com>
2021-08-13 19:07:02 -0700

    Setting up heroku environment and sending static files

--- Routes/index.js
+++ Routes/index.js
@@ -1,11 +1,10 @@
 const router = require("express").Router();
-const path = require("path");
 const apiRoutes = require("./API");
+const path = require("path");
 
 router.use("/api", apiRoutes);
-
-// router.get("*", (req, res) =>
-// 	res.sendFile(path.join(__dirname, "../client/build/index.html"))
-// );
+router.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,34 +11,16 @@ mongoose.connect("mongodb://localhost:27017/example", {
 	useCreateIndex: true,
 });
 
-const db = require("./models");
-
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// app.post("/api/test/data", (req, res) => {
-// 	console.log(req);
-// 	db.Test.create(req.body)
-// 		.then((data) => {
-// 			console.log("data", data);
-// 			res.json(data);
-// 		})
-// 		.catch((err) => console.log(err));
-// });
-
-// app.get("/api/test/data", (req, res) => {
-// 	db.Test.find({})
-// 		.then((data) => {
-// 			res.json(data);
-// 		})
-// 		.catch((err) => console.log(err));
-// });
-
-// app.use(express.static(path.join(__dirname, "public")));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+}
 
 app.listen(PORT, () =>
 	console.log(`App is listening on http://localhost:${PORT}`)
